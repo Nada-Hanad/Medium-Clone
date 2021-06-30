2df965892bcf6bb3442c080c0290eb248083bd47
Hanad-Nada <jn_hanad@esi.dz>
2021-06-30 15:48:28 +0100

    master

---
+++ src/Components/main.js
@@ -0,0 +1,16 @@
+import React from "react";
+export default function Main() {
+  return (
+    <div className="main">
+      <h1>Where good ideas find you.</h1>
+      <div className="pl">
+        <p>
+          Read and share new perspectives on just about any topic. Everyone’s
+          welcome.
+        </p>
+        <a href="somewher">Learn More</a>
+      </div>
+      <button>Get started</button>
+    </div>
+  );
+}
